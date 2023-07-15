99a80808ddb9f6bb0b742bd7a7da531f8e789c1d
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2023-07-15 16:46:56 +0200

    First release

--- README.md
+++ README.md
@@ -1,2 +1,80 @@
 # cupl.bat
-A batch file to execute WinCupl compilation and fitting from the command-line
+
+A batch file to execute WinCUPL compilation and fitting from the command-line.
+
+WinCUPL is a legacy application used to design logic for GALs (16V8, 22V10), SPLDs (Atmel's ATF16V8, ATF22V10) and CPLDs (Atmel's ATF1502, ATF1504 and ATF1508).
+However, its GUI is clunky and unreliable, and its text editor is poor.
+
+Consequently, it is better to write logic code in your own modern text editor, and run WinCUPL's command-line tools for compilation. It is much more convenient and error output is better.
+
+## How to use
+
+TL:DR; `cupl.bat FILE.pld`
+
+### Detailed instructions
+
+1. Write your logic just like you would in WinCUPL's GUI. Let's use one of WinCUPL's built in examples: `C:\Wincupl\Examples\Atmel\ADDER.PLD`. This example implements an adder using a 16V8 GAL.
+
+2. Execute `cupl.bat` as follows `cupl.bat C:\Wincupl\Examples\Atmel\ADDER.PLD`
+
+```
+C:\Wincupl\Projects>cupl.bat C:\Wincupl\Examples\Atmel\ADDER.PLD
+cuplx
+time: 0 secs
+cupla
+time: 0 secs
+cuplb
+time: 0 secs
+cuplm
+time: 0 secs
+cuplc
+time: 0 secs
+total time: 0 secs
+
+
+Normal exit
+Error Code = 0
+
+"Device G16V8 does not need fitter, skipping."
+"Done!"
+```
+
+3. The output files are available next to the `.PLD` file, including `.JED` for programming and `.DOC` for details on how the compilation went.
+
+4. If your device necessitates a fitter (ATF1502, ATF1504, ATF1508), this is executed automatically by `cupl.bat`. Take `C:\Wincupl\Examples\Atmel\TIM_CNT.PLD` as an example. This file implements a 16-bit timer/counter using an ATF1508.
+
+```
+C:\Wincupl\Projects>cupl.bat C:\Wincupl\Examples\Atmel\TIM_CNT.PLD
+cuplx
+time: 0 secs
+cupla
+time: 0 secs
+cuplb
+time: 0 secs
+cuplm
+time: 1 secs
+cuplc
+time: 0 secs
+find1508
+time: 0 secs
+total time: 1 secs
+
+
+Normal exit
+Error Code = 0
+
+Atmel ATF1508AS Fitter Version 1.8.7.8 (02-05-03)
+Copyright 1999,2000 Atmel Corporation
+---------------------------------------------------------
+ Fitter_Pass 1, Preassign = KEEP, LOGIC_DOUBLING : OFF
+ ...
+ Design fits successfully
+
+"Done!"
+```
+
+5. And additional `.FIT` file becomes available with details on how the fitting process went.
+
+### Pause
+
+If you need to execute `cupl.bat` as an editor build command, and to keep `CMD.exe` open after compilation, `cupl.bat /p` pauses execution before closing the window. For example, I have mapped `C:\Wincupl\Projects\cupl.bat "$(FULL_CURRENT_PATH)" /p` to F6 in my Notepad++ configuration.
---
+++ cupl.bat
@@ -0,0 +1,139 @@
+@echo off
+setlocal
+
+rem Usage: cupl.bat c:\design.pld [/p]
+rem /p pauses the command instead of exiting, for use in a build shortcut
+
+set CUPLPATH=C:\Wincupl
+set LIBCUPL=%CUPLPATH%\Shared\cupl.dl
+set PATH=%PATH%;%CUPLPATH%\WinCupl;%CUPLPATH%\WinCupl\Fitters;%CUPLPATH%\Shared
+
+if not exist "%~dpnx1" (
+	echo "Error: Input file '%~dpnx1' does not exist";
+	goto :error
+)
+
+rem cupl likes to execute where the file is located
+cd %~dp1
+
+rem delete build outputs if some are present
+for %%e in (abs doc fit io jed lst mx pin pla sim tt2 tt3) do (
+  if exist "%~dpn1.%%e" del /Q "%~dpn1.%%e"
+)
+
+rem get device from file
+FOR /F "tokens=2 delims=; " %%D in ('FINDSTR /RB "^Device *([a-zA-Z0-9]+) *;" "%~dpnx1"') do set DEVICE=%%D
+
+rem execute cupl
+cupl.exe -a -l -e -x -f -b -j -m2 -n %DEVICE% "%~dpnx1" || echo "Error: executing cupl.exe failed" && goto :error
+
+if not exist "%~dpn1.jed" (
+	echo "Error: jed output from cupl.exe does not exist";
+	goto :error
+)
+
+rem translate device from pld file to fitter and arguments
+rem TODO: How to make this code nicer?
+IF "%DEVICE%"=="f1502ispplcc44" (
+	SET FITTER=find1502.exe
+	SET FITTER_ARGS=-dev P1502C44 -str JTAG ON
+) ELSE IF "%DEVICE%"=="f1502plcc44" (
+	SET FITTER=find1502.exe
+	SET FITTER_ARGS=-dev P1502C44
+) ELSE IF "%DEVICE%"=="f1502isptqfp44" (
+	SET FITTER=find1502.exe
+	SET FITTER_ARGS=-dev P1502T44 -str JTAG ON
+) ELSE IF "%DEVICE%"=="f1502tqfp44" (
+	SET FITTER=find1502.exe
+	SET FITTER_ARGS=-dev P1502T44
+) ELSE IF "%DEVICE%"=="f1504ispplcc44" (
+	SET FITTER=find1504.exe
+	SET FITTER_ARGS=-dev P1504C44 -str JTAG ON
+) ELSE IF "%DEVICE%"=="f1504plcc44" (
+	SET FITTER=find1504.exe
+	SET FITTER_ARGS=-dev P1504C44
+) ELSE IF "%DEVICE%"=="f1504ispplcc68" (
+	SET FITTER=find1504.exe
+	SET FITTER_ARGS=-dev P1504C68 -str JTAG ON
+) ELSE IF "%DEVICE%"=="f1504plcc68" (
+	SET FITTER=find1504.exe
+	SET FITTER_ARGS=-dev P1504C68
+) ELSE IF "%DEVICE%"=="f1504ispplcc84" (
+	SET FITTER=find1504.exe
+	SET FITTER_ARGS=-dev P1504C84 -str JTAG ON
+) ELSE IF "%DEVICE%"=="f1504plcc84" (
+	SET FITTER=find1504.exe
+	SET FITTER_ARGS=-dev P1504C84
+) ELSE IF "%DEVICE%"=="f1504ispqfp100" (
+	SET FITTER=find1504.exe
+	SET FITTER_ARGS=-dev P1504Q100 -str JTAG ON
+) ELSE IF "%DEVICE%"=="f1504qfp100" (
+	SET FITTER=find1504.exe
+	SET FITTER_ARGS=-dev P1504Q100
+) ELSE IF "%DEVICE%"=="f1504isptqfp44" (
+	SET FITTER=find1504.exe
+	SET FITTER_ARGS=-dev P1504T44 -str JTAG ON
+) ELSE IF "%DEVICE%"=="f1504tqfp44" (
+	SET FITTER=find1504.exe
+	SET FITTER_ARGS=-dev P1504T44
+) ELSE IF "%DEVICE%"=="f1504isptqfp100" (
+	SET FITTER=find1504.exe
+	SET FITTER_ARGS=-dev P1504T100 -str JTAG ON
+) ELSE IF "%DEVICE%"=="f1504tqfp100" (
+	SET FITTER=find1504.exe
+	SET FITTER_ARGS=-dev P1504T100
+) ELSE IF "%DEVICE%"=="f1504ispplcc84" (
+	SET FITTER=find1504.exe
+	SET FITTER_ARGS=-dev P1504C84 -str JTAG ON
+) ELSE IF "%DEVICE%"=="f1504plcc84" (
+	SET FITTER=find1504.exe
+	SET FITTER_ARGS=-dev P1504C84
+) ELSE IF "%DEVICE%"=="f1508ispplcc84" (
+	SET FITTER=find1508.exe
+	SET FITTER_ARGS=-dev P1508C84 -str JTAG ON
+) ELSE IF "%DEVICE%"=="f1508plcc84" (
+	SET FITTER=find1508.exe
+	SET FITTER_ARGS=-dev P1508C84
+) ELSE IF "%DEVICE%"=="f1508ispqfp100" (
+	SET FITTER=find1508.exe
+	SET FITTER_ARGS=-dev P1508Q100 -str JTAG ON
+) ELSE IF "%DEVICE%"=="f1508qfp100" (
+	SET FITTER=find1508.exe
+	SET FITTER_ARGS=-dev P1508Q100
+) ELSE IF "%DEVICE%"=="f1508isppqfp160" (
+	SET FITTER=find1508.exe
+	SET FITTER_ARGS=-dev P1508Q160 -str JTAG ON
+) ELSE IF "%DEVICE%"=="f1508pqfp160" (
+	SET FITTER=find1508.exe
+	SET FITTER_ARGS=-dev P1508Q160
+) ELSE IF "%DEVICE%"=="f1508isptqfp100" (
+	SET FITTER=find1508.exe
+	SET FITTER_ARGS=-dev P1508T100 -str JTAG ON
+) ELSE IF "%DEVICE%"=="f1508tqfp100" (
+	SET FITTER=find1508.exe
+	SET FITTER_ARGS=-dev P1508T100
+)
+
+rem Execute fitter if needed
+IF DEFINED FITTER (
+	%FITTER% -i "%~dpn1.tt2" -CUPL %FITTER_ARGS% || echo "Error: executing fitter failed" && goto :error
+	if not exist "%~dpn1.jed" (
+		echo "Error: jed output from fitter does not exist";
+		goto :error
+	)
+) ELSE (
+	echo "Device %DEVICE% does not need fitter, skipping."
+)
+
+echo "Done!"
+if "%2"=="/p" (
+	pause
+)
+exit /B 0
+
+:error
+if "%2"=="/p" (
+	pause
+) else (
+	exit /B 1
+)
